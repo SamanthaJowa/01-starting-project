dff7ba676c4a00bc43ab5b96eae22d9acaa1830b
SamanthaJowa <sammjowa01@gmail.com>
2024-07-02 13:02:58 +0200

    viewing and canceling new task component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,16 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
-  selector: 'app-tasks',
-  standalone: true,
-  imports: [TaskComponent],
-  templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+    selector: 'app-tasks',
+    standalone: true,
+    templateUrl: './tasks.component.html',
+    styleUrl: './tasks.component.css',
+    imports: [TaskComponent, NewTaskComponent]
 })
 export class TasksComponent {
   @Input({required: true}) name!: string;
   @Input({required: true}) userId!: string;
+  isAddingTask = false;
 
   tasks = [
     {
@@ -39,6 +41,14 @@ export class TasksComponent {
 
   get selectedUserTasks(){
     return this.tasks.filter((task) => task.userId === this.userId)
+  }
+
+  onStartAddTask(){
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask(){
+    this.isAddingTask = false;
   }
 
   onCompleteTask(id: string){
